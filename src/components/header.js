ce97d5078cf56abed1659889ea6ef8a92fcf8334
BennettBishop <Bennettjamesbishop@gmail.com>
2024-07-22 23:32:33 -0700

    Better header styling

--- src/components/header.js
+++ src/components/header.js
@@ -5,29 +5,27 @@ import { Dialog, DialogPanel } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
+  { name: 'Projects', href: '#' },
+  { name: 'About Me', href: '#' },
+  { name: 'Contact Me', href: '#' },
 ]
 
 export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <header className="bg-white">
+    <header className="bg-indigo-600">
       <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
-            <img alt="" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" className="h-8 w-auto" />
-          </a>
+        <h1 className=" text-2xl font-bold tracking-tight text-white sm:text-3xl">
+                  BJB
+        </h1>
         </div>
         <div className="flex lg:hidden">
           <button
             type="button"
             onClick={() => setMobileMenuOpen(true)}
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-indigo-300"
           >
             <span className="sr-only">Open main menu</span>
             <Bars3Icon aria-hidden="true" className="h-6 w-6" />
@@ -35,14 +33,18 @@ export default function Header() {
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
           {navigation.map((item) => (
-            <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+            <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-white">
               {item.name}
             </a>
           ))}
         </div>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            Log in <span aria-hidden="true">&rarr;</span>
+          <a
+            href='https://www.linkedin.com/in/bennett-bishop'
+            onClick={() => window.location.href='https://www.linkedin.com/in/bennett-bishop'}
+            className="text-sm font-semibold leading-6 text-white"
+          >
+           LinkedIn <span aria-hidden="true">&rarr;</span>
           </a>
         </div>
       </nav>
@@ -50,14 +52,9 @@ export default function Header() {
         <div className="fixed inset-0 z-10" />
         <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img
-                alt=""
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                className="h-8 w-auto"
-              />
-            </a>
+          <h1 className=" text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+                  BJB
+        </h1>
             <button
               type="button"
               onClick={() => setMobileMenuOpen(false)}
@@ -81,12 +78,15 @@ export default function Header() {
                 ))}
               </div>
               <div className="py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Log in
-                </a>
+
+                    <a
+            onClick={() => window.location.href='https://www.linkedin.com/in/bennett-bishop'}
+            href='https://www.linkedin.com/in/bennett-bishop'
+             className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+          >
+           LinkedIn <span aria-hidden="true">&rarr;</span>
+          </a>
+        
               </div>
             </div>
           </div>
