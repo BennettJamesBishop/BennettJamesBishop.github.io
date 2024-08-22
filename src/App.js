e1fa9f75416878c4456a1bf8d7f60c285b8610df
BennettBishop <Bennettjamesbishop@gmail.com>
2024-08-21 23:55:09 -0700

    Add ScrollToTop component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/header';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Footer from './components/footer';
+import ScrollToTop from './components/ScrollToTop';
 import './App.css'
 function App() {
   return ( 
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/projects/:index" element={<Projects />} />
         </Routes>
+        <ScrollToTop />
         <Footer />
     </>
   );
